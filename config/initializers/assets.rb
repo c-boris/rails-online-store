770d8688f1ad1879e339898d36f9090753383f38
Hugo Rischmann <hugo.rischmann@me.com>
2023-06-06 12:35:09 +0200

    precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,3 +18,5 @@ vendor_path = Rails.root.join("vendor")
 Dir.glob(vendor_path.join("**/*")).each do |dir|
   Rails.application.config.assets.paths << dir if File.directory?(dir)
 end
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.css style.css animate.css slick.css slick-theme.css jquery/dist/jquery.min.js bootstrap/js/bootstrap.min.js bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js instafeed/instafeed.min.js ekko-lightbox/dist/ekko-lightbox.min.js syo-timer/build/jquery.syotimer.min.js slick/slick.min.js slick/slick-animation.min.js google-map/gmap.js )
