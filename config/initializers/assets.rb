9cf8a4a6b4fbaf745f9aa6578e69904e19f9dd17
Hugo Rischmann <hugo.rischmann@me.com>
2023-06-06 12:39:08 +0200

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.version = "1.1"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -18,5 +18,3 @@ vendor_path = Rails.root.join("vendor")
 Dir.glob(vendor_path.join("**/*")).each do |dir|
   Rails.application.config.assets.paths << dir if File.directory?(dir)
 end
-
-Rails.application.config.assets.precompile += %w( bootstrap.min.css style.css animate.css slick.css slick-theme.css jquery/dist/jquery.min.js bootstrap/js/bootstrap.min.js bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js instafeed/instafeed.min.js ekko-lightbox/dist/ekko-lightbox.min.js syo-timer/build/jquery.syotimer.min.js slick/slick.min.js slick/slick-animation.min.js google-map/gmap.js )
