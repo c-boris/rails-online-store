127fede82ac4d8a29f0710f26862059aa63f590e
Tommy <sasida@hotmail.fr>
2023-06-06 12:55:34 +0200

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
     title: Faker::Name.first_name,
     description: Faker::Lorem.sentence,
     price: rand(5.00..1000.00),
-    image_url: Faker::LoremFlickr.image(size: "20x20", search_terms: ['kitten'])
+    image_url: Faker::LoremFlickr.image(size: "500x500", search_terms: ['kitten'])
   )
 end
