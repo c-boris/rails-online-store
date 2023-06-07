0bc64e3b571196d30f4086ca515541971dee0d46
Hugalito <hugo.rischmann@me.com>
2023-06-07 14:16:23 +0200

    install mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
-KEYWORDS = ['kitten', 'cat', 'katua', 'chat', 'kitty', 'chaton', 'paws', 'whiskers', 'miaou', 'purr', 'gato', 'gata', 'katze', 'yeowl', 'meowing', 'con_mèo', 'meows', 'pisică', 'macska', 'gat']
+require 'uri'
 
-NUM_ITEMS = KEYWORDS.length
+CHAT_KEYWORDS = ['kitten', 'cat', 'katua', 'chat', 'kitty', 'chaton', 'paws', 'whiskers', 'miaou', 'purr', 'gato', 'gata', 'katze', 'yeowl', 'meowing', 'con_mèo', 'meows', 'pisică', 'macska', 'gat']
+
+NUM_ITEMS = CHAT_KEYWORDS.length
 
 NUM_ITEMS.times do
-  random_index = rand(KEYWORDS.length)
-  random_keyword = KEYWORDS[random_index]
-  KEYWORDS.delete_at(random_index)
+  random_index = rand(CHAT_KEYWORDS.length)
+  random_keyword = CHAT_KEYWORDS[random_index]
+  CHAT_KEYWORDS.delete_at(random_index)
 
+  encoded_keyword = URI.encode_www_form_component(random_keyword)
   item_image = Faker::LoremFlickr.image(
     size: "450x450",
-    search_terms: [random_keyword]
+    search_terms: [encoded_keyword]
   )
 
-  Item.create!(
+  item = Item.new(
     title: Faker::Name.first_name,
     description: Faker::Lorem.sentence,
-    price: rand(50..999.99),
-    image_url: item_image
+    price: rand(50..999.99)
   )
+
+  item.item_picture.attach(io: URI.open(item_image), filename: 'image.jpg')
+
+  item.save!
 end
