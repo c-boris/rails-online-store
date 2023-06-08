e3a24e111795e59a393937a44d3a2d544d39b3c2
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-06-08 15:21:15 +0200

    add order view + link to profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_110837) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_112727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_110837) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price"
+    t.decimal "price", precision: 10, scale: 2
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
